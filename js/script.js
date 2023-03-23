82f9d5fb3429375b3053aa42af87011f6211e5e6
barszczwiktoria <barszcz.wiktoria@gmail.com>
2023-03-23 21:11:41 +0100

    addNewTask - function arg added

--- js/script.js
+++ js/script.js
@@ -43,18 +43,18 @@
           
         for (const task of tasks) {
             htmlString += `
-    <li class="tasks__item js-tasks"> 
-    <button class="tasks__button tasks__button--toggleDone js-toggleDone">
-    ${task.done ? "✓" : ""}
-    </button>
-   <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">
-    ${task.content}</span>
-    <button class="tasks__button tasks__button--remove js-remove"> 
-    🗑
-    </button>
-    </li>
-    `;
-}
+            <li class="tasks__item js-tasks"> 
+            <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+            ${task.done ? "✓" : ""}
+            </button>
+           <span class="tasks__content ${task.done ? "tasks__content--done" : ""}">
+            ${task.content}</span>
+            <button class="tasks__button tasks__button--remove js-remove"> 
+            🗑
+            </button>
+            </li>
+            `;
+        }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
@@ -80,7 +80,7 @@
         const form = document.querySelector(".js-form");
         form.addEventListener("submit", onFormSubmit);
 
-        addNewTask();
+        addNewTask(newTaskContent);
     };
 
     init();
